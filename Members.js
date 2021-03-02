18e8b06d4955d1715c83aa70affaddf0e2346286
FelipeDominguesB <felipedom2001@hotmail.com>
2021-03-02 16:13:32 -0300

    Aprendendo uso de Middleware com moment, utilizando a classe Logger para criar um log das requisições (não salva ainda), também aprendendo a usar module.exports para exportar informações, como em Members.js

---
+++ Members.js
@@ -0,0 +1,29 @@
+const members =[
+    {
+        nome: 'Felipe',
+        idade: 19,
+        Genero: 'Masculino',
+        Cidade: 'São Paulo'
+    }, 
+    {
+        nome: 'Arthur',
+        idade: 18,
+        Genero: 'Masculino',
+        Cidade: 'São Paulo'
+    }, 
+    {
+        nome: 'Amelie',
+        idade: 25,
+        Genero: 'Feminino',
+        Cidade: 'França'
+    }, 
+    {
+        nome: 'Double-trouble',
+        idade: 20,
+        Genero: 'Outro',
+        Cidade: 'Etheria'
+    }, 
+];
+
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members.js');
+const logger = require('./middleware/logger.js')
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const members =[
-    {
-        nome: 'Felipe',
-        idade: 19,
-        Genero: 'Masculino',
-        Cidade: 'São Paulo'
-    }, 
-    {
-        nome: 'Arthur',
-        idade: 18,
-        Genero: 'Masculino',
-        Cidade: 'São Paulo'
-    }, 
-    {
-        nome: 'Amelie',
-        idade: 25,
-        Genero: 'Feminino',
-        Cidade: 'França'
-    }, 
-    {
-        nome: 'Double-trouble',
-        idade: 20,
-        Genero: 'Outro',
-        Cidade: 'Etheria'
-    }, 
-];
 
+app.use(logger);
 
 //Pega todos os membros.
 app.get('/api/members', (req, res)=>{
